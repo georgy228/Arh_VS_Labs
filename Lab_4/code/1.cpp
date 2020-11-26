1d72fc766b55adb5edbdafa1730dc72aef849d91
sekibura <51335422+sekibura@users.noreply.github.com>
2020-11-26 15:20:13 +0300

    Update 1.cpp

--- Lab_4/code/1.cpp
+++ Lab_4/code/1.cpp
@@ -5,9 +5,11 @@ int main(void)
 
     unsigned int i = 2147483647;
     bool flag = false;
-
+        // прибавляем 1 к i
     asm ( "add $1, %0\n" : "=r" (i) : "0" (i) );
+    //проверяем overfloew flag, если ==0, то переходим к метке Zero
     asm ( "jno Zero;"\
+         // если !=0 то меняем значение переменной
           "mov $0x1,%0;"\
           "Zero :"\
           : "=r" (flag) : "0" (flag) );
@@ -23,6 +25,7 @@ int main(void)
     {
         printf("The result is correct\n");
     }
+
     return 0;
 
 }
